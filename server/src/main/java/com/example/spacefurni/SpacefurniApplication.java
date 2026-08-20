5b78d32e41068dd336ca1a78999d547445167391
vinhkwang <vinhlequang5@gmail.com>
2026-08-20 21:23:28 +0700

    Init base project

---
+++ server/src/main/java/com/example/spacefurni/SpacefurniApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacefurni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacefurniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacefurniApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/spacefurni/SpacefurniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacefurni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacefurniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
