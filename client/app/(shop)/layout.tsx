1c98e35395e125a23b45eb0f2bd88ea51e1dec1d
vinhkwang <vinhlequang5@gmail.com>
2026-09-02 21:52:44 +0700

    add shop footer and shop route group layout

--- client/app/(shop)/layout.tsx
+++ client/app/(shop)/layout.tsx
@@ -1,3 +1,12 @@
+import { ShopFooter } from "@/components/layout/ShopFooter";
+import { ShopHeader } from "@/components/layout/ShopHeader";
+
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <ShopHeader />
+      <div className="flex-1">{children}</div>
+      <ShopFooter />
+    </>
+  );
 }
